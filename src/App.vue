f95ea93136811db83e1837f95be147c4cbf3ebeb
Domenico Scala <147435323+DomenicoScala@users.noreply.github.com>
2024-01-18 17:27:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+    import AppHeader from './components/AppHeader.vue'
+    import AppMain from './components/AppMain.vue'
+    import AppFooter from './components/AppFooter.vue'
+    
+    export default{
+        data(){
+            return{
+
+            };
+        },
+        methods:{
+
+        },
+        components:{
+            AppHeader,
+            AppMain,
+            AppFooter
+        }
+    }
+</script>
+
+<template>
+
+        <h1>
+            Mia App
+        </h1>
+
+        <AppHeader/>
+
+        <AppMain/>
+
+        <AppFooter/>
+
+</template>
+
+<style lang="scss">
+    @use "assets/scss/main.scss" as *;
+    @import "assets/scss/partials/reset.scss";
+</style>
